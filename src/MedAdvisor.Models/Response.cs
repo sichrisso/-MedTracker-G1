b23ea66260324b09ff9b5c205737680a3d475b5e
sichrisso <helenasolomon87@gmail.com>
2023-02-13 15:28:47 +0300

    fix conflict

--- src/MedAdvisor.Models/Response.cs
+++ src/MedAdvisor.Models/Response.cs
@@ -6,19 +6,11 @@ using System.Threading.Tasks;
 
 namespace MedAdvisor.Models
 {
-<<<<<<< HEAD
     public class Response
     {
         public int StatusCode { get; set; }
         public string ErrorMessage { get; set; }
     }
 
-=======
-   public class Response
-    {
-        public int statusCode { get; set; }
-         
-        public string statusMessage { get; set; }
-    }
->>>>>>> 54bc1f49180982b6c0b7819df64b4e933ec15f75
+  
 }
